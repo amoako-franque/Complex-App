b54d37b22a9c5c0d0f96633e2969c2b75c1a011b
armoako <frank.amoako-dev@outlook.com>
2022-01-14 19:22:29 +0000

    minor update

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,7 @@ exports.viewCreateScreen = function(req, res) {
 
 exports.create = function(req, res) {
   let post = new Post(req.body, req.session.user._id)
-  post.create().then(function(newId) {
+  post.create().then(function (newId) {
     req.flash("success", "New post successfully created.")
     req.session.save(() => res.redirect(`/post/${newId}`))
   }).catch(function(errors) {
--- router-api.js
+++ router-api.js
@@ -1,7 +1,6 @@
 const apiRouter = require('express').Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
-const followController = require('./controllers/followController')
 const cors = require("cors")
 
 apiRouter.use(cors())
